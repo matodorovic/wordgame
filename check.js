30219c40e530dc947fa3be9533d77aa7e926f5c6
Marta Anna Todorovic <martaannatodorovic@gmail,com>
2022-03-16 12:24:47 +0100

    fix of return array length and some html

--- check.js
+++ check.js
@@ -1,29 +1,29 @@
 export function checkWords(word1, word2) {
-    const newArray = [];
-    const array1 = word1.split("");
-    const array2 = word2.split("");
-  
-    let obj = {};
-  
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        obj = { letter: array2[i], result: "correct" };
-        newArray.splice(i, 0, obj);
-        array1.splice(i, 1);
-        array2.splice(i, 1);
-      } else {
-        obj = { letter: array2[i], result: "incorrect" };
-        newArray.splice(i, 0, obj);
-      }
+  const newArray = [];
+  const array1 = word1.split("");
+  const array2 = word2.split("");
+
+  let obj = {};
+
+  for (let i = 0; i < array2.length; i++) {
+    if (array1[i] === array2[i]) {
+      obj = { letter: array2[i], result: "correct" };
+      newArray.splice(i, 0, obj);
+      array1.splice(i, 1);
+      //array2.splice(i, 1);
+    } else {
+      obj = { letter: array2[i], result: "incorrect" };
+      newArray.splice(i, 0, obj);
     }
-  
-    for (let i = 0; i < array1.length; i++) {
-      if (array1.includes(array2[i]) && array1[i] !== array2[i]) {
-        obj = { letter: array2[i], result: "misplaced" };
-        newArray.splice(i, 1, obj);
-        array1.splice(i, 1);
-      }
-    }
-    console.log(newArray);
-    return newArray;
   }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1.includes(array2[i]) && array1[i] !== array2[i]) {
+      obj = { letter: array2[i], result: "misplaced" };
+      newArray.splice(i, 1, obj);
+      array1.splice(i, 1);
+    }
+  }
+  console.log(newArray);
+  return newArray;
+}
--- main.js
+++ main.js
@@ -12,19 +12,28 @@ let errorPrint = document.querySelector("#error-length");
 
 btnWord1.addEventListener("click", () => {
   word1 = document.querySelector(".chosen").value.toLowerCase();
+  if (!word1) {
+    errorPrint.innerHTML = "U need to write a word to continue."
+  } else {
+  errorPrint.innerHTML = "";
   console.log(word1, word1.length);
   btnWord1.disabled = true;
-  word1Print.append(word1);
+  word1Print.append("*****");
+  }
 });
 
 btnWord2.addEventListener("click", () => {
   word2 = document.querySelector(".guess").value.toLowerCase();
-  if (word1.length != word2.length) {
-    errorPrint.append("Words have to have same length.");
+  if (word1 === word2) {
+    document.querySelector("#won-msg").innerHTML = "Congratulations u won!";
+  } else if (word1.length != word2.length) {
+    errorPrint.innerHTML = "Words have to have same length.";
   } else {
+    errorPrint.innerHTML = "";
     console.log(word2, word2.length);
     btnWord2.disabled = true;
     word2Print.append(word2);
     checkWords(word1, word2);
+
   }
 });
